a1126ea7297b40e604aed72556281056e6e25cb8
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-03-19 19:53:40 +0545

    Create Place model and setup database

--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Place(models.Model):
+  
+    def __str__(self):
+        return self.place_name
+  
+    place_name = models.CharField(max_length=200)
+    place_desc = models.CharField(max_length=200)
+    estimated_cost = models.IntegerField()
--- travel/views.py
+++ travel/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Place
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello")
+    place_list = Place.objects.all()
+    return HttpResponse(place_list)
 
 
 def place(request):
