93ec1877835a212399b79493597cec65695f31e4
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-03-20 16:45:57 +0545

    Add templates and basic views for displaying places

--- travel/urls.py
+++ travel/urls.py
@@ -1,7 +1,13 @@
 from . import views
 from django.urls import path
 
+
+app_name = "travel"
 urlpatterns = [
+    # /travel-diary/
     path("", views.index, name="index"),
+    # /travel-diary/:id
+    path("<int:place_id>/", views.detail, name="detail"),
+    # /travel-diary/place
     path("place/", views.place, name="place"),
 ]
--- travel/views.py
+++ travel/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Place
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
     place_list = Place.objects.all()
-    return HttpResponse(place_list)
+    context = {
+        "place_list": place_list,
+    }
+    return render(request, "travel/index.html", context)
 
 
 def place(request):
     return HttpResponse("This is place")
+
+
+def detail(request, place_id):
+  place= Place.objects.get(pk=place_id)
+  context = {
+      "place": place
+  }
+  return render(request, "travel/detail.html", context)
