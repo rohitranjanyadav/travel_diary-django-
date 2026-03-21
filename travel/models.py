d3659cd67470fd49198ec7c4bb16c975676962ae
Rohit Ranjan Yadav <iamshareefg@gmail.com>
2026-03-21 19:03:25 +0545

    feat: add static assets and initial site design

--- travel/models.py
+++ travel/models.py
@@ -10,3 +10,4 @@ class Place(models.Model):
     place_name = models.CharField(max_length=200)
     place_desc = models.CharField(max_length=200)
     estimated_cost = models.IntegerField()
+    place_image = models.CharField(max_length=500, default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT36fqQUPeXvmxYZlOAzqiIFNwMOcXwRQHBgw&s")
